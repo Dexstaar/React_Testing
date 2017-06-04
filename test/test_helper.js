5be448df550725ce8e3f80dff43f204a831e9d08
dexstar <dexstaar@gmail.com>
2017-06-04 11:21:55 +0800

    .

--- test/test_helper.js
+++ test/test_helper.js
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+// Set up testing environment to run like a browser in the command line 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 global.navigator = global.window.navigator;
@@ -16,6 +17,7 @@ const $ = _$(window);
 
 chaiJquery(chai, chai.util, $);
 
+// Build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass, props = {}, state = {}) {
   const componentInstance =  TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
@@ -32,5 +34,7 @@ $.fn.simulate = function(eventName, value) {
   }
   TestUtils.Simulate[eventName](this[0]);
 };
+
+// Set up chai-jquery
 
 export {renderComponent, expect};
